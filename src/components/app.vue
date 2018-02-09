fd617b2a2c9c5de7d2b7c392daaa1621867b056b
Shigeki Suwa <suwa.shigeki@gmail.com>
2018-02-09 23:18:13 +0900

    vue app

---
+++ src/components/app.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.app-root
+  h1 HELLLLO
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('app mounted');
+  },
+};
+</script>
+
+<style lang="stylus">
+h1 {
+  font-weight: normal;
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue/dist/vue.esm.js';
+import App from './components/app.vue';
+
+const app = new Vue({
+  el: '#app',
+  components: { App },
+});
